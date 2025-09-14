bca4fda961d725289cafcc4db935932d5082d8bc
David Martinez <Martinezd44@montclair.edu>
2025-09-14 03:21:08 -0400

    Add text preprocessing

---
+++ preprocess.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import string
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+def preprocess_text(text):
+    text = text.lower()
+    text = ''.join([char for char in text if char not in string.punctuation])
+    tokens = word_tokenize(text)
+    stop_words = set(stopwords.words('english'))
+    tokens = [word for word in tokens if word not in stop_words and word.isalpha()]
+    return ' '.join(tokens)
+
+data = pd.read_csv('spam.csv', encoding='latin-1')
+data = data[['v1', 'v2']]
+data.columns = ['label', 'text']
+data['clean_text'] = data['text'].apply(preprocess_text)
+print("Sample cleaned text:")
+print(data[['text', 'clean_text']].head())
+data.to_csv('processed_data.csv', index=False)  # Save
